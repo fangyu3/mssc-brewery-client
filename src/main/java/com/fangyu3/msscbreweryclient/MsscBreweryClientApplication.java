41eb53a3a13cb9ab3fe7651fec5523fee9650973
fangyu3 <tofang23@gmail.com>
2020-11-18 01:29:55 -0500

    Initial commit

---
+++ src/main/java/com/fangyu3/msscbreweryclient/MsscBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.fangyu3.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fangyu3/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangyu3.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
